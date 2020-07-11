ed9a894d7e86682c55e3de361efa639d6e3b899d
sid779 <siddhartha779@gmail.com>
2020-07-12 02:44:07 +0530

    Bot upgraded1.5

--- app.py
+++ app.py
@@ -34,10 +34,6 @@ def respond():
 
    else:
        try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
            # reply with a photo to the name the user sent,
            # note that you can send photos by url and telegram will fetch it for you
            bot.sendMessage(chat_id=chat_id, text="Hi honey ❤️\n Why you said" + text, reply_to_message_id=msg_id)
